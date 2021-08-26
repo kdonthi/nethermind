c0e8d665891859c1300d0b72a0a07a00d7fe7bbf
Marcin Sobczak <marcindsobczak@gmail.com>
2021-08-26 13:52:01 +0200

    fix

--- src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V62/BlockHeadersMessage.cs
+++ src/Nethermind/Nethermind.Network/P2P/Subprotocols/Eth/V62/BlockHeadersMessage.cs
@@ -14,6 +14,7 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with the Nethermind. If not, see <http://www.gnu.org/licenses/>.
 
+using System.Linq;
 using Nethermind.Core;
 
 namespace Nethermind.Network.P2P.Subprotocols.Eth.V62
@@ -34,6 +35,6 @@ namespace Nethermind.Network.P2P.Subprotocols.Eth.V62
             BlockHeaders = blockHeaders;
         }
 
-        public override string ToString() => $"{nameof(BlockHeadersMessage)}({BlockHeaders?.Length ?? 0})({BlockHeaders[0]})";
+        public override string ToString() => $"{nameof(BlockHeadersMessage)}({BlockHeaders?.Length ?? 0})({BlockHeaders?.FirstOrDefault()})";
     }
 }
